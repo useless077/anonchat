2408d29b73bca4a765cf0bec3efc326386ec545e
useless077 <95554539+useless077@users.noreply.github.com>
2025-09-23 14:05:06 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,28 @@
 # utils.py
 from datetime import datetime
 from pyrogram.types import Message
-from config import Config
+import config
 from database.users import Database
 
 async def log_message(app, sender_id, sender_name, msg: Message):
     text = f"[{datetime.utcnow().isoformat()}Z]\nFrom: <a href='tg://user?id={sender_id}'>{sender_name}</a>"
+    
     if msg.text:
         text += f"\nText: {msg.text}"
-        await app.send_message(Config.LOG_CHANNEL, text, parse_mode="html")
+        await app.send_message(config.LOG_CHANNEL, text, parse_mode="html")
+    
     elif msg.photo:
-        await app.send_photo(Config.LOG_CHANNEL, msg.photo[-1].file_id)
+        await app.send_photo(config.LOG_CHANNEL, msg.photo[-1].file_id)
+    
     elif msg.sticker:
-        await app.send_sticker(Config.LOG_CHANNEL, msg.sticker.file_id)
+        await app.send_sticker(config.LOG_CHANNEL, msg.sticker.file_id)
+    
     elif msg.animation:
-        await app.send_animation(Config.LOG_CHANNEL, msg.animation.file_id)
+        await app.send_animation(config.LOG_CHANNEL, msg.animation.file_id)
+    
     else:
-        await app.send_message(Config.LOG_CHANNEL, f"Other type from {sender_name}", parse_mode="html")
+        await app.send_message(
+            config.LOG_CHANNEL,
+            f"Other type from {sender_name}",
+            parse_mode="html"
+        )
