1e3d69e7ced57819408aed9088e6982eb09417d6
useless077 <95554539+useless077@users.noreply.github.com>
2025-09-23 11:40:25 +0530

    Update webhook_app.py

--- webhook_app.py
+++ webhook_app.py
@@ -1,21 +1,10 @@
-from pyrogram import Client
-from config import Config
-import asyncio
+from flask import Flask
+app = Flask(__name__)
 
-bot = Client(
-    "anon_chat_bot",
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    sleep_threshold=60,   # handle minor clock drift
-    workers=4,
-)
+@app.route('/')
+def hello_world():
+    return 'Bisal_Files'
 
-async def main():
-    await bot.start()
-    print("Bot started!")
-    # Keep the bot running forever
-    await asyncio.Event().wait()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run()
