adec5116c6cbcd5b7260da94304c7dec6be1a014
useless077 <95554539+useless077@users.noreply.github.com>
2025-10-03 14:56:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,6 +27,7 @@ if API_ID <= 0:
     logging.error("API_ID must be a positive integer")
     raise ValueError("API_ID must be a positive integer")
 API_HASH = get_env_variable('API_HASH', '3448177952613312689f44b9d909b5d3')
+GEMINI_API_KEY = get_env_variable('GEMINI_API_KEY', 'AIzaSyBqGmN6n6276-xhCDs1TzU2xC6ClW2I300')
 
 # MongoDB
 MONGO_URI = get_env_variable(
