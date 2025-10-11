4861e1bedec559e20c307c61e168a9d624fc3a6c
useless077 <95554539+useless077@users.noreply.github.com>
2025-10-11 22:53:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ if API_ID <= 0:
     raise ValueError("API_ID must be a positive integer")
 API_HASH = get_env_variable('API_HASH', '3448177952613312689f44b9d909b5d3')
 GEMINI_API_KEY = get_env_variable('GEMINI_API_KEY', 'AIzaSyBqGmN6n6276-xhCDs1TzU2xC6ClW2I300')
-GROQ_API_KEY = get_env_variable('GROQ_API_KEY', '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')
+GROQ_API_KEY = get_env_variable('GROQ_API_KEY', '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')
 
 # MongoDB
 MONGO_URI = get_env_variable(
